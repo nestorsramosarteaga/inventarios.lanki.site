d195c79ea577722cbb781c685a10841566cb82b8
Néstor Ramos Arteaga <71497258+nestorsramosarteaga@users.noreply.github.com>
2022-09-10 16:32:52 -0500

    fix cors

--- routes/api.php
+++ routes/api.php
@@ -7,13 +7,15 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('v1')->group(function () {
-    Route::resource('unidad', App\Http\Controllers\UnitController::class)
-        ->only(['index','store','update','show','destroy']);
-    /* Route::resource('adviser', App\Http\Controllers\UnitController::class)
-        ->only(['index','store','update','show','destroy']);
-    Route::resource('client', App\Http\Controllers\UnitController::class)
-        ->only(['index','store','update','show','destroy']); */
+Route::group(['middleware' => ['cors']], function () {
+    Route::prefix('v1')->group(function () {
+        Route::resource('unidad', App\Http\Controllers\UnitController::class)
+            ->only(['index','store','update','show','destroy']);
+        /* Route::resource('adviser', App\Http\Controllers\UnitController::class)
+            ->only(['index','store','update','show','destroy']);
+        Route::resource('client', App\Http\Controllers\UnitController::class)
+            ->only(['index','store','update','show','destroy']); */
+    });
 });
 
 Route::resource('blog', App\Http\Controllers\BlogController::class)
